1e14c7a1cc4544b12913f8cb6a9139df3fdf1962
经典鸡翅 <zhq_java@163.com>
2022-09-24 21:52:25 +0800

    feat:分布式锁

--- ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
+++ ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class RedisUtil {
 
@@ -12,6 +14,19 @@ public class RedisUtil {
 
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
+    }
+
+    public boolean setNx(String key, String value, Long time, TimeUnit timeUnit){
+        return redisTemplate.opsForValue().setIfAbsent(key,value,time,timeUnit);
+    }
+
+    public String get(String key){
+        String value = (String) redisTemplate.opsForValue().get(key);
+        return value;
+    }
+
+    public boolean del(String key){
+        return redisTemplate.delete(key);
     }
 
 }
--- ape-user/src/main/java/com/jingdianjichi/user/controller/TestController.java
+++ ape-user/src/main/java/com/jingdianjichi/user/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jingdianjichi.user.controller;
 
+import com.jingdianjichi.redis.util.RedisShareLockUtil;
 import com.jingdianjichi.redis.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,6 +13,9 @@ public class TestController {
     @Autowired
     private RedisUtil redisUtil;
 
+    @Autowired
+    private RedisShareLockUtil redisShareLockUtil;
+
     @GetMapping("/test")
     public String test() {
         return "Hello World";
@@ -20,6 +24,12 @@ public class TestController {
     @GetMapping("/testRedis")
     public void testRedis() {
         redisUtil.set("name", "鸡翅");
+    }
+
+    @GetMapping("/testRedisLock")
+    public void testRedisLock() {
+        boolean result = redisShareLockUtil.lock("jichi", "1231231", 100000L);
+        System.out.println(result);
     }
 
 }
