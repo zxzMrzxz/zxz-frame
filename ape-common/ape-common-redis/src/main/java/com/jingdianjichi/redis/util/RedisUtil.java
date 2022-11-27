fb086298e47b9ff4ebe6429dbed38179c1f6cb8b
经典鸡翅 <zhq_java@163.com>
2022-11-27 16:46:14 +0800

    feat：read

--- ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
+++ ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
@@ -1,32 +1,61 @@
 package com.jingdianjichi.redis.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * @Author: ChickenWing
+ * @Description: RedisUtil工具类
+ * @DateTime: 2022/11/27 16:00
+ */
 @Component
+@Slf4j
 public class RedisUtil {
 
-    @Autowired
+    @Resource
     private RedisTemplate redisTemplate;
+
+    private static final String CACHE_KEY_SEPARATOR = ".";
+
+    /**
+     * 构建缓存key
+     */
+    public String buildKey(String... strObjs) {
+        return Stream.of(strObjs).collect(Collectors.joining(CACHE_KEY_SEPARATOR));
+    }
+
+    /**
+     * 是否存在key
+     */
+    public boolean exist(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 删除key
+     */
+    public boolean del(String key) {
+        return redisTemplate.delete(key);
+    }
 
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
 
-    public boolean setNx(String key, String value, Long time, TimeUnit timeUnit){
-        return redisTemplate.opsForValue().setIfAbsent(key,value,time,timeUnit);
+    public boolean setNx(String key, String value, Long time, TimeUnit timeUnit) {
+        return redisTemplate.opsForValue().setIfAbsent(key, value, time, timeUnit);
     }
 
-    public String get(String key){
+    public String get(String key) {
         String value = (String) redisTemplate.opsForValue().get(key);
         return value;
     }
 
-    public boolean del(String key){
-        return redisTemplate.delete(key);
-    }
 
 }
