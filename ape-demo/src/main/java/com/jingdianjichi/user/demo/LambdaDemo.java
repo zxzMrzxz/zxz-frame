a9db0977058ef2df7cfd5e5a3f8b76327cd2db45
经典鸡翅 <zhq_java@163.com>
2022-12-17 17:00:38 +0800

    feat：增加表达式demo

--- ape-demo/src/main/java/com/jingdianjichi/user/demo/LambdaDemo.java
+++ ape-demo/src/main/java/com/jingdianjichi/user/demo/LambdaDemo.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @Author: ChickenWing
@@ -28,6 +31,27 @@ public class LambdaDemo {
             skuVO.setSkuName(sku.getSkuName());
             return skuVO;
         });
+    }
+
+    /**
+     * 将list转为map的demo
+     */
+    public void listToMap() {
+        List<SkuInfo> skuInfoList = new LinkedList<>();
+        SkuInfo skuInfo = new SkuInfo();
+        skuInfo.setSkuId(1L);
+        skuInfo.setSkuName("商品1");
+        skuInfo.setPrice(new BigDecimal("0.01"));
+        // 转为id和实体的map
+        Map<Long, SkuInfo> resultMap = skuInfoList.stream()
+                .collect(Collectors.toMap(SkuInfo::getSkuId, Function.identity(), (key1, key2) -> key2));
+
+        //转为属性的map
+        Map<Long, String> resultMap1 = skuInfoList.stream()
+                .collect(Collectors.toMap(SkuInfo::getSkuId, SkuInfo::getSkuName, (key1, key2) -> key2));
+
+        //分组map
+        Map<Long, List<SkuInfo>> resultMap2 = skuInfoList.stream().collect(Collectors.groupingBy(SkuInfo::getSkuId));
     }
 
     @Data
