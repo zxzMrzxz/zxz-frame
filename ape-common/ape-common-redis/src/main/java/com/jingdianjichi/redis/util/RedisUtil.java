4c672b82dcc2b914010e47a70a1c57518ee474d4
经典鸡翅 <zhq_java@163.com>
2023-01-09 00:12:54 +0800

    feat:redis实现延迟队列

--- ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
+++ ape-common/ape-common-redis/src/main/java/com/jingdianjichi/redis/util/RedisUtil.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -55,6 +56,44 @@ public class RedisUtil {
     public String get(String key) {
         String value = (String) redisTemplate.opsForValue().get(key);
         return value;
+    }
+
+    public Boolean zAdd(String key, String value, Long score) {
+        return redisTemplate.opsForZSet().add(key, value, Double.valueOf(String.valueOf(score)));
+    }
+
+    public Long countZset(String key) {
+        return redisTemplate.opsForZSet().size(key);
+    }
+
+    public Set<String> rangeZset(String key, long start, long end) {
+        return redisTemplate.opsForZSet().range(key, start, end);
+    }
+
+    public Long removeZset(String key, Object value) {
+        return redisTemplate.opsForZSet().remove(key, value);
+    }
+
+    public void removeZsetList(String key, Set<String> value) {
+        value.stream().forEach((val) -> {
+            redisTemplate.opsForZSet().remove(key, val);
+        });
+    }
+
+    public Double score(String key, Object value) {
+        return redisTemplate.opsForZSet().score(key, value);
+    }
+
+    public Set<String> rangeByScore(String key, long start, long end) {
+        return redisTemplate.opsForZSet().rangeByScore(key, Double.valueOf(String.valueOf(start)), Double.valueOf(String.valueOf(end)));
+    }
+
+    public Object addScore(String key, Object obj, double score) {
+        return redisTemplate.opsForZSet().incrementScore(key, obj, score);
+    }
+
+    public Object rank(String key, Object obj) {
+        return redisTemplate.opsForZSet().rank(key, obj);
     }
 
 
