0bf4ef4b1327853a89b67004a24851623f030832
经典鸡翅 <zhq_java@163.com>
2023-01-15 23:20:42 +0800

    feat:异步工具类

--- ape-common/ape-common-tool/src/main/java/com/jingdianjichi/tool/CompletableFutureUtils.java
+++ ape-common/ape-common-tool/src/main/java/com/jingdianjichi/tool/CompletableFutureUtils.java
@@ -1,21 +1,27 @@
 package com.jingdianjichi.tool;
 
 import org.slf4j.Logger;
+
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Author: ChickenWing
- * @Description: 异步future工具类封装
- * @DateTime: 2023/1/8 22:24
+ * 异步future工具类封装
+ *
+ * @author: ChickenWing
+ * @date: 2023/1/15
  */
 public class CompletableFutureUtils {
 
+    /**
+     * 获取future返回结果
+     */
     public static <T> T getResult(Future<T> future, long timeout, TimeUnit timeUnit, T defaultValue, Logger logger) {
         try {
             return future.get(timeout, timeUnit);
         } catch (Exception e) {
-            logger.error("get future error,defaultValue:{}", defaultValue, e);
+            logger.error("CompletableFutureUtils.getResult.error:{},defaultValue:{}", e.getMessage(), e);
+            logger.error("CompletableFutureUtils.getResult.error.returnDefaultValue:{}", defaultValue);
             return defaultValue;
         }
     }
